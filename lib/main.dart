56b7042559329ea8ad61a96784af709ac825ada3
Fluke <suphakit_1122@hotmail.co.th>
2022-09-23 22:53:13 +0700

    format code

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 void main() => runApp(const MyApp());
-
-enum ButtonState { init, loading, done }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   static const String _title = '';
